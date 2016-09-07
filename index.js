e03d1011dcbc3fc33d4bb689d1282cc4232ef333
Mahesh Sankhala <maheshsainni@gmail.com>
2016-09-07 17:54:20 +0530

    gpio code added.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var gpio = require('rpi-gpio');
+var gpio = require('pi-gpio');
 
-gpio.setup(7, gpio.DIR_OUT, write);
+gpio.open(7, 'output');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -14,6 +14,7 @@ io.on('connection', function(socket){
     gpio.write(7, state, function(err) {
       if (err) throw err;
       console.log('Written to pin');
+      gpio.close(7);
     });
     // console.log(state);
     // console.log(typeof(state));
@@ -24,8 +25,3 @@ io.on('connection', function(socket){
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-
-function write() {
-  if (err) throw err;
-  console.log('Setup pin callback');
-}
