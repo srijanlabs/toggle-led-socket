b7aee3a7061e0466fd3eda62640339d475921507
Mahesh Sankhala <maheshsainni@gmail.com>
2016-09-07 17:02:56 +0530

    gpio code fixed.

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var gpio = require('rpi-gpio');
 
+gpio.setup(7, gpio.DIR_OUT, write);
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   socket.on('toggle', function(state){
-    gpio.setup(7, gpio.DIR_OUT, write(state));
+    gpio.write(7, state, function(err) {
+      if (err) throw err;
+      console.log('Written to pin');
+    });
     // console.log(state);
     // console.log(typeof(state));
     // io.emit('state', state);
@@ -20,9 +25,7 @@ http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-function write(state) {
-  gpio.write(7, state, function(err) {
-    if (err) throw err;
-    console.log('Written to pin');
-  });
+function write() {
+  if (err) throw err;
+  console.log('Setup pin callback');
 }
