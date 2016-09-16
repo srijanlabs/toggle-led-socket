49a7f269fdc3f6595c04c62d2bf40da1de5f5f7e
Mahesh Sankhala <maheshsainni@gmail.com>
2016-09-16 10:50:21 +0530

    gpio code updated.

--- index.js
+++ index.js
@@ -10,13 +10,10 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   socket.on('toggle', function(state){
     gpio.open(7, "output", function(err) {   // Open pin 7 for output
-        gpio.write(7, 1, function() {      // Set pin 7 high (1)
-            gpio.close(7);           // Close pin 7
-        });
+      gpio.write(7, state, function() {      // Set pin 7 high (1)
+        //gpio.close(7);           // Close pin 7
+      });
     });
-    // console.log(state);
-    // console.log(typeof(state));
-    // io.emit('state', state);
   });
 });
 
